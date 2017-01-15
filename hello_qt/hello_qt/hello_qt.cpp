35e35fff70e05a5ea5e764cb189231582167bd1f
Julia Baki <bakijulia@gmail.com>
2017-01-15 15:53:04 +0100

    First steps in QT. Hello QT!

---
+++ hello_qt/hello_qt/hello_qt.cpp
@@ -0,0 +1,12 @@
+#include "hello_qt.h"
+
+hello_qt::hello_qt(QWidget *parent)
+	: QMainWindow(parent)
+{
+	ui.setupUi(this);
+}
+
+hello_qt::~hello_qt()
+{
+
+}
---
+++ hello_qt/hello_qt/main.cpp
@@ -0,0 +1,10 @@
+#include "hello_qt.h"
+#include <QtWidgets/QApplication>
+
+int main(int argc, char *argv[])
+{
+	QApplication a(argc, argv);
+	hello_qt w;
+	w.show();
+	return a.exec();
+}
