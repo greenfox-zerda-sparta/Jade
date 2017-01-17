c5937e4f450bbc8aac75beced16698dc2a7345ed
Oszvaldb <nagyongonoszi@gmail.com>
2017-01-17 11:51:21 +0100

    c5
    Signed-off-by: Oszvaldb <nagyongonoszi@gmail.com>

--- practice/practice/cheer.cpp
+++ practice/practice/cheer.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <string>
 
-void cheer();
+void cheer(std::string, std::string);
 	
 int main() {
-	cheer();
+	cheer("Hello", " Jade!" );
 	return 0;
 }
 
-void cheer() {
-	std::cout << "c++" << std::endl;
+void cheer(std::string param1, std::string param2) {
+	std::cout <<param1 << param2 << std::endl;
 }
