9b3c6ecf559efaddc1943fad44b36599dcca4109
Julia Baki <bakijulia@gmail.com>
2017-01-18 13:59:19 +0100

    json parse test

---
+++ test/json_second_try/json_second_try/json_second_try.cpp
@@ -0,0 +1,12 @@
+#include "json_second_try.h"
+
+json_second_try::json_second_try(QWidget *parent)
+    : QMainWindow(parent)
+{
+    ui.setupUi(this);
+}
+
+json_second_try::~json_second_try()
+{
+
+}
