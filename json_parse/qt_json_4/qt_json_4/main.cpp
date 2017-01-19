3a9767671463610a67acfd077a507ad501af32ce
Julia Baki <bakijulia@gmail.com>
2017-01-19 15:35:20 +0100

    refactor: new Class, JsonParser

--- json_parse/qt_json_4/qt_json_4/main.cpp
+++ json_parse/qt_json_4/qt_json_4/main.cpp
@@ -1,34 +1,16 @@
-#include "qt_json_4.h"
-#include "FileReader.h"
-#include <QtWidgets/QApplication>
-#include <string>
-#include <fstream>
 #include <iostream>
-#include <QJsonDocument>
-#include <QJsonParseError>
-#include <QByteArray>
-#include <QJsonObject>
-#include <QJsonArray>
+#include "FileReader.h"
+#include "JsonParser.h"
 
 int main(int argc, char *argv[]) {
   FileReader fileReader;
   std::string content = fileReader.readFromFileToString("test.json");
 
-  QStringList title;
-  QJsonParseError *error = Q_NULLPTR;
-  QByteArray byteArray(content.c_str(), content.length());
-  QJsonDocument jsonResponse = QJsonDocument::fromJson(byteArray, error);
-  QJsonObject jsonObject = jsonResponse.object();
-  QJsonArray jsonArray = jsonObject["news"].toArray();
-  foreach(const QJsonValue & value, jsonArray) {
-    QJsonObject obj = value.toObject();
-    title.append(obj["title"].toString());
-  }
+  JsonParser jsonParser;
+  QStringList titles = jsonParser.parseFromStringToQStringList(content);
 
-  for (int i = 0; i < title.size(); ++i) {
-    std::cout << title.at(i).toUtf8().constData() << std::endl;
+  for (int i = 0; i < titles.size(); ++i) {
+    std::cout << titles.at(i).toUtf8().constData() << std::endl;
   }
-
- 
   return 0;
 }
