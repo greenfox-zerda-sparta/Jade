4efb04d25b4e7a770bdd3566833706f7cb6690ed
Balazs Makszim <b.makszim@hotmail.com>
2017-01-20 10:06:02 +0100

    Juli overwritten

---
+++ jade_reader/jade_reader/jade_reader.h
@@ -0,0 +1,19 @@
+#ifndef JADE_READER_H
+#define JADE_READER_H
+
+#include <QtWidgets/QMainWindow>
+#include "ui_jade_reader.h"
+
+class jade_reader : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    jade_reader(QWidget *parent = 0);
+    ~jade_reader();
+
+private:
+    Ui::jade_readerClass ui;
+};
+
+#endif // JADE_READER_H
