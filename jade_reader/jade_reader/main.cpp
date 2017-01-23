33c7270d16c5c8ce356020af1e85cf44422b510c
oszi <nagyongonoszi@gmail.com>
2017-01-23 11:52:45 +0100

    refactor: Application class

--- jade_reader/jade_reader/main.cpp
+++ jade_reader/jade_reader/main.cpp
@@ -1,31 +1,8 @@
-#include <iostream>
-#include <QLabel>
-#include <QtWidgets>
-#include <QVBoxLayout>
-#include <QtGui>
-#include "FileReader.h"
-#include "JsonParser.h"
-#include "LabelCreator.h"
+#include "Application.h"
 
 int main(int argc, char *argv[]) {
-  FileReader fileReader;
-  std::string content = fileReader.readFromFileToString("test.json");
-  JsonParser jsonParser;
-  std::vector <Article*> articles;
-  articles = jsonParser.parseFromStringToArticleVector(content);
-
-  QApplication app(argc, argv);
-  QWidget *window = new QWidget();
-  QVBoxLayout *layout = new QVBoxLayout();
-  layout->setSizeConstraint(QLayout::SetMaximumSize);
-  LabelCreator labelCreator;
-
-  for (int i = 0; i < articles.size(); ++i) {
-    layout->addWidget(labelCreator.createLabelFromArticle(articles[i]));
-  }
-
-  window->setLayout(layout);
-
-  window->show();
-  return app.exec();
+	Application application;
+	QApplication app(argc, argv);
+	application.run();
+	return app.exec();
 }
