2ba95eb0b5085f492af5ab49d78673975c6d7e0f
oszi <nagyongonoszi@gmail.com>
2017-01-25 16:21:19 +0100

    new feat: description label is now resposive. new class: flowlayout

--- jade_reader/jade_reader/LabelCreator.cpp
+++ jade_reader/jade_reader/LabelCreator.cpp
@@ -8,5 +8,9 @@ LabelCreator::~LabelCreator() {}
 QLabel* LabelCreator::createLabelFromQString(QString input) {
   QLabel* qLabel = new QLabel(input);
   qLabel->setWordWrap(true);
+  QSizePolicy sizePolicy1(QSizePolicy::Expanding, QSizePolicy::Expanding);
+  //sizePolicy1.setHeightForWidth(qLabel->sizePolicy().hasHeightForWidth());
+  qLabel->setSizePolicy(sizePolicy1);
+  qLabel->setFrameStyle(QFrame::Panel | QFrame::Sunken);
   return qLabel;
 }
--- jade_reader/jade_reader/LayoutCreator.cpp
+++ jade_reader/jade_reader/LayoutCreator.cpp
@@ -1,9 +1,11 @@
 #include "LayoutCreator.h"
+#include "flowlayout.h"
 #include <QPushButton>
+#include <QLayout>
+#include <QWidgetItem>
 
 
 LayoutCreator::LayoutCreator() {}
-
 
 LayoutCreator::~LayoutCreator() {}
 
@@ -11,14 +13,17 @@ QGridLayout* LayoutCreator::createLayout(Article* article) {
   LabelCreator labelCreator;
 
   QGridLayout * layout = new QGridLayout();
+  FlowLayout* titleDate = new FlowLayout();
   QPushButton *pushButton1 = new QPushButton();
   QPushButton *pushButton2 = new QPushButton();
  // void QGridLayout::addWidget(QWidget *widget, int fromRow, int fromColumn, int rowSpan, int columnSpan, Qt::Alignment alignment = Qt::Alignment())
-  layout->addWidget(labelCreator.createLabelFromQString(article->getTitle()),1,1,1,-1, Qt::AlignLeft);
-  layout->addWidget(labelCreator.createLabelFromQString(article->getCreated()), 2, 1, 1, 1, Qt::AlignLeft);
-  layout->addWidget(labelCreator.createLabelFromQString(article->getDescription()), 2, 1, 2, -1, Qt::AlignLeft);
-  layout->addWidget(pushButton1, 1, 2, 1, 1);
-  layout->addWidget(pushButton2, 2, 2, 1, 1);
+  //layout->addItem(titleDate, 1, 1, 1, 1, Qt::AlignLeft);
+  layout->addWidget(labelCreator.createLabelFromQString(article->getTitle()), 0, 0, 1, 1, Qt::AlignLeft);
+  layout->addWidget(labelCreator.createLabelFromQString(article->getCreated()), 1, 0, 1, 1, Qt::AlignLeft);
+  layout->addWidget(labelCreator.createLabelFromQString(article->getDescription()), 2, 0, 1, -1, Qt::AlignLeft);
+  layout->setColumnStretch(0, 99);
+  layout->addWidget(pushButton1, 0, 1);
+  layout->addWidget(pushButton2, 1, 1);
 
   return layout;
 }
