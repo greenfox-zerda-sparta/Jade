6322750f3806085c1f73e28dd825fd9cdd3b04d9
Julia Baki <bakijulia@gmail.com>
2017-01-30 16:49:32 +0100

    new feat (UserLoginWindow) : "Sign In" and "Sign Up" Button

--- jade_reader/jade_reader/UserLoginWindow.cpp
+++ jade_reader/jade_reader/UserLoginWindow.cpp
@@ -4,10 +4,18 @@
 UserLoginWindow::UserLoginWindow(QWidget *parent) : QDialog(parent) {
   emailLineEdit = new QLineEdit(parent);
   passwordLineEdit = new QLineEdit(parent);
+
   QFormLayout* formLayout = new QFormLayout;
+  setWindowTitle(tr("JADE READER"));
   formLayout->addRow(tr("&Email:"), emailLineEdit);
   formLayout->addRow(tr("&Password:"), passwordLineEdit);
-  setLayout(formLayout);
+
+  signInButton = new QPushButton(tr("Sign In"));
+  signUpButton = new QPushButton(tr("Sign Up"));
+  QVBoxLayout *mainLayout = new QVBoxLayout(this);
+  mainLayout->addLayout(formLayout);
+  mainLayout->addWidget(signInButton);
+  mainLayout->addWidget(signUpButton);
 }
 
 UserLoginWindow::~UserLoginWindow() {}
--- jade_reader/jade_reader/UserLoginWindow.h
+++ jade_reader/jade_reader/UserLoginWindow.h
@@ -2,13 +2,15 @@
 
 #include <QDialog>
 #include <QLineEdit>
+#include <QPushButton>
 
 class UserLoginWindow : public QDialog {
   Q_OBJECT
 private:
   QLineEdit* emailLineEdit;
   QLineEdit* passwordLineEdit;
-  
+  QPushButton* signInButton;
+  QPushButton* signUpButton;
 public:
   explicit UserLoginWindow(QWidget* parent = Q_NULLPTR);
   ~UserLoginWindow();
