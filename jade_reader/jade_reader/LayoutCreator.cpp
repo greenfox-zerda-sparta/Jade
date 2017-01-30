195186cebd332932122cda3c44a8108cbfcf8eee
oszi <nagyongonoszi@gmail.com>
2017-01-30 09:26:46 +0100

    minor changes

--- jade_reader/jade_reader/LayoutCreator.cpp
+++ jade_reader/jade_reader/LayoutCreator.cpp
@@ -16,13 +16,14 @@ QGridLayout* LayoutCreator::createLayout(Article* article) {
   FlowLayout* titleDate = new FlowLayout();
   QPushButton *pushButton1 = new QPushButton();
   QPushButton *pushButton2 = new QPushButton();
- // void QGridLayout::addWidget(QWidget *widget, int fromRow, int fromColumn, int rowSpan, int columnSpan, Qt::Alignment alignment = Qt::Alignment())
+  //void QGridLayout::addWidget(QWidget *widget, int fromRow, int fromColumn, int rowSpan, int columnSpan, Qt::Alignment alignment = Qt::Alignment())
   //layout->addItem(titleDate, 1, 1, 1, 1, Qt::AlignLeft);
   layout->addWidget(labelCreator.createLabelFromQString(article->getTitle()), 0, 0, 1, 1, Qt::AlignLeft);
   layout->addWidget(labelCreator.createLabelFromQString(article->getCreated()), 1, 0, 1, 1, Qt::AlignLeft);
   layout->addWidget(labelCreator.createLabelFromQString(article->getDescription()), 2, 0, 1, -1, Qt::AlignLeft);
   layout->setColumnStretch(0, 99);
   layout->addWidget(pushButton1, 0, 1);
+  pushButton1->setText("Go to link");
   layout->addWidget(pushButton2, 1, 1);
 
   return layout;
