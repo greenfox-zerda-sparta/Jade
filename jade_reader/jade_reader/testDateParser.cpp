0f08427e8c975f6c680ece58a1420655133d3a04
Balazs Makszim <b.makszim@hotmail.com>
2017-01-30 10:10:35 +0100

    fix: dateParser tester now tests for english months

--- jade_reader/jade_reader/testDateParser.cpp
+++ jade_reader/jade_reader/testDateParser.cpp
@@ -2,9 +2,8 @@
 #include "Article.h"
 #include "QDebug"
 
-void TestDateParser::checkTest1() {
+void TestDateParser::checkIfDateInEnglish() {
   Article article("a", "b", 1485266405);
-  QString hungarian = QLatin1String("január 24, 2017");
   QString english = ("January 24, 2017");
-  QVERIFY(article.getCreated() == hungarian || article.getCreated() == english);
+  QCOMPARE(article.getCreated(), english);
 }
