42a87ef5aaba51ee359304f5a5f056ebf190ab71
oszi <nagyongonoszi@gmail.com>
2017-01-31 14:48:28 +0100

    refactor: LayoutCreator & ArticleLayout class

--- jade_reader/jade_reader/LayoutCreator.cpp
+++ jade_reader/jade_reader/LayoutCreator.cpp
@@ -14,17 +14,33 @@ LayoutCreator::LayoutCreator() {}
 
 LayoutCreator::~LayoutCreator() {}
 
+ArticleLayout::ArticleLayout(Article* _article) {
+  article = _article;
+  LabelCreator labelCreator;
+  layout = new QGridLayout();
+  layout->setObjectName(article->getUrl());
+  QPushButton *pushButton1 = new QPushButton();
+  QPushButton *pushButton2 = new QPushButton();
+  layout->addWidget(labelCreator.createLabelFromQString(article->getTitle()), 0, 0, 1, 1, Qt::AlignLeft);
+  layout->addWidget(labelCreator.createLabelFromQString(article->getCreated()), 1, 0, 1, 1, Qt::AlignLeft);
+  layout->addWidget(labelCreator.createLabelFromQString(article->getFeedName()), 0, 1, 1, 1, Qt::AlignLeft);
+  layout->addWidget(labelCreator.createLabelFromQString(article->getUrl()), 1, 1, 1, 1, Qt::AlignLeft);
+  layout->addWidget(labelCreator.createLabelFromQString(article->getDescription()), 2, 0, 1, -1, Qt::AlignLeft);
+  layout->setColumnStretch(0, 99);
+  layout->addWidget(pushButton1, 0, 2);
+  pushButton1->setText("Go to link");
+  connect(pushButton1, &QAbstractButton::clicked, this, &ArticleLayout::newBrowserWindow);
+  layout->addWidget(pushButton2, 1, 2);
+}
+
 ArticleLayout* LayoutCreator::createLayout(Article* article) {
   ArticleLayout* al = new ArticleLayout(article);
   return al;
 }
 
-void LayoutCreator::openNewBrowserWindow(QGridLayout* layout) {
-
+void ArticleLayout::newBrowserWindow() {
+  QWidget* widget = Q_NULLPTR;
+  QWebEngineView *view = new QWebEngineView(widget);
+  view->load(QUrl(article->getUrl()));
+  view->show();
 }
-
-void LayoutCreator::clickOpenBrowser() {
-//  openNewBrowserWindow();
-}
-
-
--- jade_reader/jade_reader/LayoutCreator.h
+++ jade_reader/jade_reader/LayoutCreator.h
@@ -15,34 +15,11 @@ class Application;
 class ArticleLayout : public QObject{
   Q_OBJECT
 public:
-  ArticleLayout(Article* _article) {
-    article = _article;
-    LabelCreator labelCreator;
-    layout = new QGridLayout();
-    layout->setObjectName(article->getUrl());
-    QPushButton *pushButton1 = new QPushButton();
-    QPushButton *pushButton2 = new QPushButton();
-    layout->addWidget(labelCreator.createLabelFromQString(article->getTitle()), 0, 0, 1, 1, Qt::AlignLeft);
-    layout->addWidget(labelCreator.createLabelFromQString(article->getCreated()), 1, 0, 1, 1, Qt::AlignLeft);
-    layout->addWidget(labelCreator.createLabelFromQString(article->getFeedName()), 0, 1, 1, 1, Qt::AlignLeft);
-    layout->addWidget(labelCreator.createLabelFromQString(article->getUrl()), 1, 1, 1, 1, Qt::AlignLeft);
-    layout->addWidget(labelCreator.createLabelFromQString(article->getDescription()), 2, 0, 1, -1, Qt::AlignLeft);
-    layout->setColumnStretch(0, 99);
-    layout->addWidget(pushButton1, 0, 2);
-    pushButton1->setText("Go to link");
-    connect(pushButton1, &QAbstractButton::clicked, this, &ArticleLayout::clickOpenBrowser);
-    layout->addWidget(pushButton2, 1, 2);
-  }
+  ArticleLayout(Article* _article);
   QGridLayout* layout;
   Article* article;
   public slots:
-  void clickOpenBrowser() {
-    QWidget* widget = Q_NULLPTR;
-    QWebEngineView *view = new QWebEngineView(widget);
-    view->load(QUrl(article->getUrl()));
-    view->show();
-  }
-
+  void newBrowserWindow();
 };
 
 class LayoutCreator: public QObject{
@@ -52,7 +29,4 @@ public:
   LayoutCreator();
   ~LayoutCreator();
   ArticleLayout* createLayout(Article*);
-  void openNewBrowserWindow(QGridLayout*);
-public slots:
-void clickOpenBrowser();
 };
