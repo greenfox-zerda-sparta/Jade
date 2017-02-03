5bacbbeeb168c6bc25a0c99b5affbeb00f7f1b19
Balazs Makszim <b.makszim@hotmail.com>
2017-02-03 10:40:23 +0100

    Something changed

--- jade_reader/jade_reader/RequestManager.h
+++ jade_reader/jade_reader/RequestManager.h
@@ -4,14 +4,12 @@
 
 class RequestManager : public QObject {
   Q_OBJECT
-private:
+public:
   QNetworkAccessManager* manager;
   QNetworkReply* reply;
   QString postRequest(QString _url, QString _email, QString _password);
-public:
   RequestManager();
   ~RequestManager();
-
   QString getFeed();
   QString postLogin(QString _email, QString _password);
   QString postSignup(QString _email, QString _password);
