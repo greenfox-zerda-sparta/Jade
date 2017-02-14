05855dd65e063f9f7fcb5b1db6cfaf5ef1e836c8
Julia Baki <bakijulia@gmail.com>
2017-02-14 22:08:34 +0100

    new class AuthResponse: can store the data of server response after login and sign up

---
+++ jade_reader/jade_reader/AuthResponse.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <QObject>
+#include <QString>
+
+class AuthResponse : public QObject {
+  Q_OBJECT
+  Q_PROPERTY(QString result READ getResult WRITE setResult);
+  Q_PROPERTY(QString message READ getMessage WRITE setMessage);
+  Q_PROPERTY(QString token READ getToken WRITE setToken);
+private:
+  QString result;
+  QString message;
+  QString token;
+public:
+  Q_INVOKABLE AuthResponse();
+  QString getResult();
+  void setResult(QString result);
+  QString getMessage();
+  void setMessage(QString message);
+  QString getToken();
+  void setToken(QString token);
+};
+
