0389a2225b41ec510a56c8bbcac141f49787839b
Julia Baki <bakijulia@gmail.com>
2017-02-15 13:53:24 +0100

    resolve conflicts

--- jade_reader/jade_reader/AuthenticationService.cpp
+++ jade_reader/jade_reader/AuthenticationService.cpp
@@ -19,24 +19,13 @@ AuthenticationService::AuthenticationService(QSharedPointer<HttpRequest> httpReq
 
 void AuthenticationService::postLogin(QString _email, QString _password) {
   logger->info("post Login");
-  QString param = jsonParser->postLoginMessagetoJson(_email, _password);
+  PostData* postData = new PostData(_email, _password);
+  QString param = jsonParser->toJson((QObject*)postData);
   httpRequest->postRequest(Config::SERVERURL + Config::LOGINPATH, param);
 }
 
 void AuthenticationService::postSignup(QString _email, QString _password) {
   logger->info("post Sign Up");
-  postRequest(Config::SERVERURL + Config::SIGNUPPATH, _email, _password);
-}
-
-void AuthenticationService::postRequest(QString _url, QString _email, QString _password) {
-  QUrl url(_url);
-  QNetworkRequest request = QNetworkRequest(url);
-  request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
-  PostData* postData = new PostData(_email, _password);
-  QString param = jsonParser->toJson((QObject*)postData);
-  logger->info(param.toUtf8());
-  connect(manager.data(), SIGNAL(finished(QNetworkReply*)), this, SLOT(replyFinished(QNetworkReply*)));
-  manager->post(request, param.toUtf8());
   PostData* postData = new PostData(_email, _password);
   QString param = jsonParser->toJson((QObject*)postData);
   httpRequest->postRequest(Config::SERVERURL + Config::SIGNUPPATH, param);
