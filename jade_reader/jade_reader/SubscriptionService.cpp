73c7f8a3ce1423251beb982bf9bd58c42b2d34e6
Balazs Makszim <b.makszim@hotmail.com>
2017-02-22 13:31:41 +0100

    new feature: add SubCreator class which can create new subscribe

--- jade_reader/jade_reader/SubscriptionService.cpp
+++ jade_reader/jade_reader/SubscriptionService.cpp
@@ -4,9 +4,12 @@
 
 SubscriptionService::SubscriptionService(QSharedPointer<HttpRequest> httpRequest) :
   httpRequest(httpRequest),
-  subLister(new SubLister(httpRequest)){
+  subLister(new SubLister(httpRequest)),
+  subCreator(new SubCreator(httpRequest)) {
   connect(this, SIGNAL(listSubscriptions()), this, SLOT(includePath()));
+  connect(this, SIGNAL(createSubscription(QJsonObject)), this, SLOT(includePath(QJsonObject)));
   connect(subLister.data(), SIGNAL(listReady(QJsonObject)), this, SLOT(listReady(QJsonObject)));
+  connect(subCreator.data(), SIGNAL(listReady(QJsonObject)), this, SLOT(createReady(QJsonObject)));
 }
 
 void SubscriptionService::includePath() {
@@ -14,6 +17,16 @@ void SubscriptionService::includePath() {
   subLister->listSubsciptions(path);
 }
 
+void SubscriptionService::includePath(QJsonObject json) {
+  QString path = Config::SUBSCRIBEPATH + "?token=" + FileHandler::readFile("token.txt");
+  subCreator->createSubscription(path, json);
+}
+
+
 void SubscriptionService::listReady(QJsonObject) {
+
+}
+
+void SubscriptionService::createReady(QJsonObject) {
 
 }
--- jade_reader/jade_reader/SubscriptionService.h
+++ jade_reader/jade_reader/SubscriptionService.h
@@ -3,6 +3,7 @@
 #include <QScopedPointer>
 #include "HttpRequest.h"
 #include "SubLister.h"
+#include "SubCreator.h"
 #include "JsonParser.h"
 #include "Logger.h"
 
@@ -10,12 +11,16 @@ class SubscriptionService : public QObject {
   Q_OBJECT
 signals:
   void listSubscriptions();
+  void createSubscription(QJsonObject);
 private slots:
   void includePath();
+  void includePath(QJsonObject);
   void listReady(QJsonObject);
+  void createReady(QJsonObject);
 private:
   QSharedPointer<HttpRequest> httpRequest;
   QScopedPointer<SubLister> subLister;
+  QScopedPointer<SubCreator> subCreator;
 public:
   SubscriptionService(QSharedPointer<HttpRequest>);
 };
